fe23ddb144c85ca54b0408b1d7b1aeda739dae5e
Restyled.io <commits@restyled.io>
2020-10-01 19:05:31 +0000

    Restyled by autopep8

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,18 +1,18 @@
-# Python program for implementation of Bubble Sort 
+# Python program for implementation of Bubble Sort
 
-def bubbleSort(arr): 
-	n = len(arr) 
 
-	# Traverse through all array elements 
-	for i in range(n-1): 
-	# range(n) also work but outer loop will repeat one time more than needed. 
+def bubbleSort(arr):
+    n = len(arr)
 
-		# Last i elements are already in place 
-		for j in range(0, n-i-1): 
+    # Traverse through all array elements
+    for i in range(n-1):
+        # range(n) also work but outer loop will repeat one time more than needed.
 
-			# traverse the array from 0 to n-i-1 
-			# Swap if the element found is greater 
-			# than the next element 
-			if arr[j] > arr[j+1] : 
-				arr[j], arr[j+1] = arr[j+1], arr[j] 
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
